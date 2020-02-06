5561a178ecbe4db2a91f0eaa1ace18acc5831771
Anaru Herbert <anaruherbert@gmail.com>
2020-02-07 00:47:30 +1000

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Anaru Herbert on 7/2/20.
+//  Copyright © 2020 Anaru Herbert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
